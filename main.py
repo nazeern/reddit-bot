e7c1d6eb624a552d16e05e8feed7c065bb32c451
Nitin Nazeer <nitin.nazeer@gmail.com>
2024-03-03 22:21:02 -0800

    Google Cloud TTS Integration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from google.cloud import texttospeech
 from itertools import islice
 import praw
+
+TTS_ENABLED=False
 
 # Capture top Reddit submission's title and text, and the top N comments
 reddit = praw.Reddit(
@@ -22,6 +25,31 @@ for comment in islice((x for x in top_submission.comments if not x.stickied), 3)
     for i, reply in enumerate(replies):
         print(f"Reply {i}:\n", reply.body)
 
+# Capture screenshots of the Reddit data gathered
 
-# Turn this Reddit information into audio clips using TTS
+
+# Turn this Reddit information into audio clips using Google Cloud TTS
+if TTS_ENABLED:
+
+    client = texttospeech.TextToSpeechClient()
+
+    synthesis_input = texttospeech.SynthesisInput(text=top_submission.title)
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-au",
+        name="en-AU-Polyglot-1",
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3,
+        effects_profile_id=["handset-class-device"]
+    )
+
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    with open("tts_audio/clip0.mp3", "wb") as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
 
